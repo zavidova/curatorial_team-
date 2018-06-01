108141bf9b1676aa280810dde612f754e102a229
zavidova <inklover29>
2018-06-01 14:04:09 +0200

    copied into another folder (for the night stylesheet)

---
+++ testing_time/js/timer.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	var d = new Date();
+	var n = d.getHours();
+	if (n > 19 || n < 6)
+	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
+	  document.body.className = "night";
+	else if (n > 16 && n < 19)
+	  // If time is between 4PM – 7PM sunset theme to ‘body’
+	  document.body.className = "sunset";
+	else
+	  // Else use ‘day’ theme
+	  document.body.className = "day";
+});
